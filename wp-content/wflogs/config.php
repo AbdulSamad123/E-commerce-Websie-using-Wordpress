2f1d971be643078f10840b7984f9d71ce26a2784
Abdul Samad <Samad0182@gmail.com>
2021-02-20 04:28:44 -0800

    Add Security

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1614427921;s:7:"authKey";s:64:"+[rf3PjGLern~7/]#O!HX8pZv#!B>QQ8e~eFc!dSE-[P1n ^08t0*1U9%c`<.wkS";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3308;}
